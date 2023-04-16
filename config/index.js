13ea2fdf777da7860a9eb08ba5f162f8e0a30609
nimrod aviam <nimrod.a6@gmail.com>
2023-04-16 16:11:07 +0300

    if (process.env.NODE_ENV === 'production')

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 var config
 console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
 
-if (false && process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   config = require('./prod')
 } else {
   config = require('./dev')
