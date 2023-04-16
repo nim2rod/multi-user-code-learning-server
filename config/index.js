4faf8f26653a4e9c6a5d5bd7c1cec3070757d60c
nimrod aviam <nimrod.a6@gmail.com>
2023-04-16 15:48:46 +0300

    always develompent

--- config/index.js
+++ config/index.js
@@ -1,11 +1,9 @@
 var config
 console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
 
-// if (false && process.env.NODE_ENV === 'production') {
-if (process.env.NODE_ENV === 'production') {
+if (false && process.env.NODE_ENV === 'production') {
   config = require('./prod')
 } else {
-
   config = require('./dev')
 }
 module.exports = config
